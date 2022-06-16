2900c33fe1973a1c6b89058a33f59e60fa2fb549
Ana Carolina Bizinoto <acbsnutri@gmail.com>
2022-06-15 22:43:38 -0300

    starting js code

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+    const input = document.querySelector("#nova-tarefa-input")
+    const tarefa = document.querySelector("#nova-tarefa-submeter")
+    const lista = document.querySelector("#tarefas")
+}) 
+
+
+function adicionarTarefa () {
+
+}
+
+function deletarTarefa () {
+
+}
