6dc1a159bfb10a0b68b76f6b4780396892b95821
Ana Carolina Bizinoto <acbsnutri@gmail.com>
2022-06-16 21:04:48 -0300

    js code

--- script.js
+++ script.js
@@ -1,14 +1,62 @@
 window.addEventListener('load', () => {
-    const input = document.querySelector("#nova-tarefa-input")
-    const tarefa = document.querySelector("#nova-tarefa-submeter")
-    const lista = document.querySelector("#tarefas")
-}) 
+	const form = document.querySelector("#nova-tarefa");
+	const input = document.querySelector("#nova-tarefa-input");
+	const list_el = document.querySelector("#tarefas");
 
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
 
-function adicionarTarefa () {
+		const task = input.value;
 
-}
+		const task_el = document.createElement('div');
+		task_el.classList.add('tarefa');
 
-function deletarTarefa () {
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('conteudo');
 
-}
+		task_el.appendChild(task_content_el);
+
+		const task_input_el = document.createElement('input');
+		task_input_el.classList.add('texto');
+		task_input_el.type = 'texto';
+		task_input_el.value = task;
+		task_input_el.setAttribute('readonly', 'readonly');
+
+		task_content_el.appendChild(task_input_el);
+
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('funcoes');
+		
+		const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add('editar');
+		task_edit_el.innerText = 'Editar';
+
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('deletar');
+		task_delete_el.innerText = 'Deletar';
+
+		task_actions_el.appendChild(task_edit_el);
+		task_actions_el.appendChild(task_delete_el);
+
+		task_el.appendChild(task_actions_el);
+
+		list_el.appendChild(task_el);
+
+		input.value = '';
+
+		task_edit_el.addEventListener('click', (e) => {
+			if (task_edit_el.innerText.toLowerCase() == "editar") {
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				task_input_el.focus();
+			} else {
+				task_edit_el.innerText = "Editar";
+				task_input_el.setAttribute("readonly", "readonly");
+			}
+		});
+
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
+	});
+});
